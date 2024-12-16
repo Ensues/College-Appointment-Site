ad9f76decdd70b3e74a37f3a250ca61f2ea4a047
Ensues <janssenquiambao@gmail.com>
2024-12-17 07:35:09 +0800

    total booking, danger button disabled

--- WebProg-FT-Case-Study/docs/calendar.php
+++ WebProg-FT-Case-Study/docs/calendar.php
@@ -1,4 +1,4 @@
-f<?php
+<?php
 session_start(); 
 
 if (!isset($_SESSION['user_id'])) {
@@ -6,30 +6,14 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 function build_calendar($month, $year) {
-
     // Database connection
     $mysqli = new mysqli('localhost', 'root', '', 'booking_system');
-    /*
-    $stmt = $mysqli -> prepare('select * from bookings where MONTH(date) = ? AND YEAR(date) = ?');
-    $stmt -> bind_param('ss', $month, $year);
-    $bookings = array();
-    if($stmt->execute()){
-        $result = $stmt -> get_result();
-        if($result->num_rows > 0){
-            while($row = $result -> fetch_assoc()){
-                $bookings[] = $row['date'];
-            }
-            $stmt->close();
-        }
-    }
-    */
+
     $sql = "SELECT appointment_date, appointment_time, status FROM appointments WHERE office_window = ? AND status = 'available' ORDER BY appointment_date, appointment_time";
-    $bookings = array();
     $window = $_GET['window'] ?? '';
     $stmt = $mysqli->prepare($sql);
     $stmt->bind_param("s", $window);
     $bookings = array();
-
 
     // Initializing days of the week
     $daysOfWeek = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
@@ -37,29 +21,28 @@ function build_calendar($month, $year) {
     // Getting first day of the month
     $firstDayOfMonth = mktime(0,0,0,$month,1,$year);
 
-    //Getting number of days in the month
+    // Getting number of days in the month
     $numberDays = date('t', $firstDayOfMonth);
 
-    //Getting info about the first day of the month
+    // Getting info about the first day of the month
     $dateComponents = getdate($firstDayOfMonth);
 
-    //Getting the name of this month
+    // Getting the name of this month
     $monthName = $dateComponents['month'];
 
-    //Getting the index value 0-6 of the first day of this month
+    // Getting the index value 0-6 of the first day of this month
     $dayOfWeek = $dateComponents['wday'];
 
-    //Getting current date
+    // Getting current date
     $dateToday = date('Y-m-d');
 
-    //Time Navigations
+    // Time Navigations
     $prev_month = date("m", mktime(0,0,0,$month - 1, 1, $year));
     $prev_year = date("Y", mktime(0,0,0,$month - 1, 1, $year));
     $next_month = date("m", mktime(0,0,0,$month + 1, 1, $year));
     $next_year = date("Y", mktime(0,0,0,$month + 1, 1, $year));
 
-    
-    //Creating the HTML table
+    // Creating the HTML table
     $calendar = "<center><h2>$monthName $year</h2></center>";
     $calendar .= "<div class='button-container' style='text-align: center; margin-top: 10px;'>";
     $calendar .= "<a class='btn btn-primary' href='?month=".$prev_month."&year=".$prev_year."' style='margin-right: 10px;margin-bottom: 10px;'>Prev Month</a>";
@@ -70,30 +53,29 @@ function build_calendar($month, $year) {
     $calendar .= "<table class='table table-bordered'>";
     $calendar .= "<tr>";
 
-    //Creating the calendar headers
+    // Creating the calendar headers
     foreach($daysOfWeek as $day){
         $calendar .= "<th>$day</th>";
     }
 
     $calendar .= "</tr><tr>";
 
-    //The variable $dayOfWeek will make sure that there must be only 7 columns on our table
+    // The variable $dayOfWeek will make sure that there must be only 7 columns on our table
     if($dayOfWeek > 0){
         for($k=0;$k<$dayOfWeek;$k++){
             $calendar .= "<td class='empty'></td>";
         }
     }
 
-    //Initiating day counter
+    // Initiating day counter
     $currentDay = 1;
 
-    //Getting month number
-
+    // Getting month number
     $month = str_pad($month,2,"0", STR_PAD_LEFT);
 
     while($currentDay <= $numberDays){
 
-        //if seventh column(saturday) reached, start a new row
+        // if seventh column(saturday) reached, start a new row
         if($dayOfWeek == 7){
             $dayOfWeek = 0;
             $calendar .= "</tr><tr>";
@@ -106,34 +88,28 @@ function build_calendar($month, $year) {
         $eventNum = 0;
         $today = $date==date('Y-m-d')?"today":"";
 
-        // If... else loop that determines the availability of the day (Workdays Only > Present and Future Days > Remaining Available)
         if($dayName == 'saturday' || $dayName == 'sunday' || $dayName == 'monday'){
             $calendar.="<td class='$today'><h4>$currentDayRel</h4> <a class='btn btn-danger btn-xs'>Closed Office</a>";
         }elseif($date<date('Y-m-d')){
             $calendar.="<td class='$today'><h4>$currentDayRel</h4> <a class='btn btn-danger btn-xs'>N/A</a>";
         }else{
             $totalbookings = checkSlots($mysqli, $date);
-            // We supposed to use this to limit how much people can book a day to fill it all up but tbh doubt it will ever realistically fill up
-            // Have tested it already and it works, you can change 54 to 1 and book a timeslot to check yourself
-            if($totalbookings == 54){
+            if($totalbookings == 48){
                 $calendar.="<td class='$today'><h4>$currentDayRel</h4> <a href='#' class='btn btn-danger btn-xs'>All booked</a>";
             }else{
-                // If changing the total bookings available per day, change this
-                $availableslots = 54 - $totalbookings;
+                $availableslots = 48 - $totalbookings;
                 $calendar.="<td class='$today'><h4>$currentDayRel</h4> <a href='book-time.php?date=".$date."&window=".$window."' class='btn btn-success btn-xs'>Book</a> <small><i>$availableslots slots left</i></small>";
             }
-            
         }
-        
+
         $calendar.= "</td>";
 
-        //Incrementing counters
+        // Incrementing counters
         $currentDay++;
         $dayOfWeek++;
     }
 
-    //Completing the row of the last week in month, if necessary
-
+    // Completing the row of the last week in month, if necessary
     if($dayOfWeek < 7){
         $remainingDays = 7-$dayOfWeek;
         for($i=0;$i<$remainingDays;$i++){
@@ -144,10 +120,7 @@ function build_calendar($month, $year) {
     $calendar .= "</tr></table>";
 
     return $calendar;
-    
 }
-
-
 
 function checkSlots($mysqli, $date){
     $stmt = $mysqli -> prepare('select * from bookings where date = ?');
@@ -183,7 +156,7 @@ function checkSlots($mysqli, $date){
         <header class="header">
             <a class="logo"> <img src="images/tsu-seal.png"> TSU <span>Registrar</span></a>
         </header>
-        <div class="container" style="padding-top: 0px">
+        <div class="container" style="padding-top: 10px">
             <div class="row">
                 <div class="col-md-12">
 
